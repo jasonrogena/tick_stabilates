495f66adcab6f25b9c06a611b0989a333b31945c
Jason Rogena <jasonrogena@gmail.com>
2013-08-29 09:13:07 +0300

    Javascript doc added to methods in tick_stabilates.js

--- js/tick_stabilates.js
+++ js/tick_stabilates.js
@@ -191,7 +191,9 @@ function TickStabilates() {
  * Note that this method is synchronous and therefore the time to execution of anything called after this method
  * is dependant on the time of execution of this method
  * 
- * @returns JsonObject, null
+ * @param {String} dataURL : the URL from where the json object will be fetched
+ * 
+ * @return JsonObject
  */
 TickStabilates.prototype.fetchDatabaseData = function (dataURL) {
    var result = null;
@@ -207,11 +209,12 @@ TickStabilates.prototype.fetchDatabaseData = function (dataURL) {
 };
 
 /**
- * This method fetches the searchable values from the stabilates, parasites and materials objects
+ * This method fetches the searchable values from the stabilates, parasites and frozenMaterial objects
  * 
- * Note that the items are added to the searchIndexes array in the order of preference
+ * Note that the items are added to the searchIndexes array in the order of preference.
+ * The stabilates, parasites and frozenMaterial objects are arrays
  * 
- * @returns Array
+ * @return Array
  */
 TickStabilates.prototype.getSearchIndexes = function () {
    var searchIndexes = new Array();
@@ -256,7 +259,7 @@ TickStabilates.prototype.getSearchIndexes = function () {
  * This method uses the RColor library (rcolor.js)
  * It returns an array of hexadecimal numbers representing colors
  * 
- * @returns Array
+ * @return Array
  */
 TickStabilates.prototype.getParasiteColors = function () {
    var parasiteColors = new Array();
@@ -323,7 +326,11 @@ TickStabilates.prototype.createYAxisLabels = function () {
 /**
  * This method calculates the Y axis position using the index of a frozen material in the frozenMaterial object
  * 
- * @returns var (Integer)
+ * Note that the frozenMaterial object is an array
+ * 
+ * @param {Integer} materialIndex : the index of the material in the frozenMaterial object
+ * 
+ * @return Integer
  */
 TickStabilates.prototype.getYAxisPosition = function (materialIndex) {
   var materialHeight = this.windowHeight / this.frozenMaterial.length;
@@ -349,7 +356,9 @@ TickStabilates.prototype.createXAxisLabels = function () {
 /**
  * This method calculates the X axis position using the index of a year where 0 is the index of the first year
  * 
- * @returns var (Integer)
+ * @param {Integer} yearIndex : the index of the year where 0 is the index of the first year
+ * 
+ * @return Integer
  */
 TickStabilates.prototype.getXAxisPosition = function (yearIndex) {
    var yearDiff = (this.endDate.getYear() + 1) - this.startDate.getYear();
@@ -382,7 +391,7 @@ TickStabilates.prototype.createDownloadLink = function () {
 /**
  * This method creates the csv text for download
  * 
- * @returns URI
+ * @return URI
  */
 TickStabilates.prototype.getDownloadText = function () {
    var csv="stabilate_no, date_prepared, number_in_tank, parasite, stock, material_frozen, source, origin, source_species_id, experiment_no, vol_prepared, medium_used, cryoprotectant, no_stored, unit, colour, ticks_ground, ticks_ml, mean_infect, infected_acin, storage_loc, stabilate_test, testing_experiment, testing_date, stabilate_history, stabilate_passages, remarks\n";
@@ -396,6 +405,15 @@ TickStabilates.prototype.getDownloadText = function () {
    return encodeURI(csv);
 };
 
+/**
+ * This method gets the name of a parasite using the parasite's index in the parasites object
+ * 
+ * Note that the parasites object is an array
+ * 
+ * @param {Integer} parasiteID : the index of the parasite in the parasites object
+ * 
+ * @return String
+ */
 TickStabilates.prototype.getParasiteName = function (parasiteID) {
    var parasitesSize = this.parasites.length;
    for(var i = 0; i < parasitesSize; i++) {
@@ -405,6 +423,15 @@ TickStabilates.prototype.getParasiteName = function (parasiteID) {
    }
 };
 
+/**
+ * This method gets the name of a material using the material's index in the frozenMaterial object
+ * 
+ * Note that the frozenMaterial object is an array
+ * 
+ * @param {Integer} materialID
+ * 
+ * @return String
+ */
 TickStabilates.prototype.getMaterialName = function (materialID) {
    var materialsSize = this.frozenMaterial.length;
    for(var i = 0; i < materialsSize; i++) {
@@ -414,6 +441,9 @@ TickStabilates.prototype.getMaterialName = function (materialID) {
    }
 };
 
+/**
+ * This method creates the Theme link in the page
+ */
 TickStabilates.prototype.createThemeLink = function () {
    this.body.append("div")
                .attr("class","theme")
@@ -435,6 +465,11 @@ TickStabilates.prototype.createThemeLink = function () {
                   });
 };
 
+/**
+ * This method creates the search field in the page
+ * 
+ * @return D3 Object
+ */
 TickStabilates.prototype.createSearchBox = function () {
    return this.body.append("input")
                .style("z-index",11)
@@ -466,6 +501,11 @@ TickStabilates.prototype.createSearchBox = function () {
                            
 };
 
+/**
+ * This method creates search suggestions using the text in the search field
+ * 
+ * @param {DOM Object} searchBox
+ */
 TickStabilates.prototype.setSearchSuggestions = function (searchBox) {
    window.d3.tickStabilatesObject.searchSuggestionBox.selectAll(".suggestion").remove();
    if(searchBox.value.length > 1) {
@@ -487,6 +527,11 @@ TickStabilates.prototype.setSearchSuggestions = function (searchBox) {
    }
 };
 
+/**
+ * This method initializes the searching process
+ * 
+ * @param {DOM Object} searchBox
+ */
 TickStabilates.prototype.search = function (searchBox) {
    window.d3.tickStabilatesObject.canvas.selectAll("circle").style("opacity", 0);
    window.d3.tickStabilatesObject.doParasiteSearch(searchBox);
@@ -495,6 +540,13 @@ TickStabilates.prototype.search = function (searchBox) {
    
 };
 
+/**
+ * This method checks whether the value in the search field is in the parasites object
+ * 
+ * Note that the parasites object is an array
+ * 
+ * @param {DOM Object} searchBox
+ */
 TickStabilates.prototype.doParasiteSearch = function (searchBox) {
    var parasitesSize = window.d3.tickStabilatesObject.parasites.length;
    for(var i = 0; i < parasitesSize; i++) {
@@ -510,6 +562,13 @@ TickStabilates.prototype.doParasiteSearch = function (searchBox) {
    }
 };
 
+/**
+ * This method checks whether the value in the search field is in the frozenMaterial object
+ * 
+ * Note that the frozenMaterial object is an array
+ * 
+ * @param {DOM Object} searchBox
+ */
 TickStabilates.prototype.doMaterialSearch = function (searchBox) {
    var materialsSize = window.d3.tickStabilatesObject.frozenMaterial.length;
    for(var i = 0; i < materialsSize; i++) {
@@ -525,6 +584,13 @@ TickStabilates.prototype.doMaterialSearch = function (searchBox) {
    }
 };
 
+/**
+ * This method checks whether the value in the search field is in the stabilates object
+ * 
+ * Note that the stabilates object is an array
+ * 
+ * @param {DOM Object} searchBox
+ */
 TickStabilates.prototype.doStabilateSearch = function (seachBox) {
    window.d3.tickStabilatesObject.canvas.selectAll("circle").each( function(d) {
       var thisCircle=d3.select(this);
@@ -573,6 +639,11 @@ TickStabilates.prototype.doStabilateSearch = function (seachBox) {
    });
 };
 
+/**
+ * This method creates a canvas on which search suggestions are added
+ * 
+ * @return D3 Object
+ */
 TickStabilates.prototype.createSearchSuggestionBox = function () {
   return this.body.append("div")
                .attr("class","searchSuggestion")
@@ -584,6 +655,12 @@ TickStabilates.prototype.createSearchSuggestionBox = function () {
                .on("mouseout",function() { window.d3.tickStabilatesObject.ignoreFocus = false; });
 };
 
+/**
+ * This method generates what is shown on the tooltip depending on where the cursor is on the screen
+ * 
+ * @param {Integer} x : the cursor's X coordinate
+ * @param {Integer} y : the cursor's Y coordinate
+ */
 TickStabilates.prototype.setTooltip = function (x, y) {
    var startYear = window.d3.tickStabilatesObject.startDate.getYear()+1900;
    var yearDiff = (window.d3.tickStabilatesObject.endDate.getYear() + 1) - window.d3.tickStabilatesObject.startDate.getYear();
@@ -598,6 +675,13 @@ TickStabilates.prototype.setTooltip = function (x, y) {
                                           .style("top",y+"px");
 };
 
+/**
+ * This method calculates the year represented by an X coordinate
+ * 
+ * @param {Integer} x : the X coordinate of which you want to determine the year
+ * 
+ * @return Integer
+ */
 TickStabilates.prototype.getYear = function (x) {
    var startYear = window.d3.tickStabilatesObject.startDate.getYear()+1900;
    var yearDiff = (window.d3.tickStabilatesObject.endDate.getYear() + 1) - window.d3.tickStabilatesObject.startDate.getYear();
@@ -605,12 +689,25 @@ TickStabilates.prototype.getYear = function (x) {
    return a + startYear;
 };
 
+/**
+ * This method calculates the index of a frozen_material represented by an Y coordinate
+ * 
+ * @param {Integer} y : the Y coordinate of which you want to determine the frozen_material index
+ * 
+ * @return Integer
+ */
 TickStabilates.prototype.getMaterialIndex = function (y) {
    var numberOfMaterials = window.d3.tickStabilatesObject.frozenMaterial.length;
    var materialHeight = window.d3.tickStabilatesObject.windowHeight / numberOfMaterials;
    return parseInt(y / materialHeight, 10);
 };
 
+/**
+ * This method zooms in on a particular sector in the UI
+ * 
+ * @param {Integer} year : The year represented by the sector
+ * @param {Integer} materialIndex : The index of the material in the frozenMaterial object represeted by the sector
+ */
 TickStabilates.prototype.zoomInOnSector = function (year,materialIndex) {
    //(year,startDate.getYear()+1900,yearDiff,materialIndex,materials,canvas,originalXs,originalYs,zoomedCircles,body,sideMenuTitle,numberOfTickStabilates,numberOfParasites,toolTip,parasites,parasitesPresent,parasiteColors)
    var startYear = window.d3.tickStabilatesObject.startDate.getYear() + 1900;
@@ -790,11 +887,33 @@ TickStabilates.prototype.zoomInOnSector = function (year,materialIndex) {
    }
 };
 
+/**
+ * This method calculates the radius of a zoomed in circle
+ * 
+ * This method's sole purpose is to scale down on the effects of the number of stabilates in storage on the radius of the
+ * circle representing the stabilate so that some of the circles are not extremely large  and others extremely tiny.
+ * Scalling down is done using the linear model 'y = mx + c' where:
+ *    y = the radius of the circle
+ *    m = 20/49
+ *    x = number of stabilates in storage
+ *    c = 225/49
+ * 
+ * @param {Integer} x : this is the number of stabilates in storage
+ * 
+ * @return Integer
+ */
 TickStabilates.prototype.getRadius = function (x) {
    var y = x * (20 / 49);
    return y + (225 / 49);
 };
 
+/**
+ * This method returns the color corresponding to a parasite
+ * 
+ * @param {String} parasiteName
+ * 
+ * @return Color
+ */
 TickStabilates.prototype.getColorUsingName = function (parasiteName) {
    var parasitesSize = window.d3.tickStabilatesObject.parasites.length;
    for(var x = 0; x < parasitesSize; x++) {
@@ -804,6 +923,11 @@ TickStabilates.prototype.getColorUsingName = function (parasiteName) {
    }
 };
 
+/**
+ * This method contains logic on zooming out from a zoomed in sector
+ * 
+ * Refer to the zoomInOnSector method
+ */
 TickStabilates.prototype.zoomOut = function () {
    window.d3.tickStabilatesObject.canvas.selectAll(".sideMenu").remove();
    window.d3.tickStabilatesObject.body.selectAll(".sideMenuTitle").transition()
@@ -869,6 +993,13 @@ TickStabilates.prototype.zoomOut = function () {
                                           .style("opacity",1);
 };
 
+/**
+ * This method returns the color corresponding to a parasite
+ * 
+ * @param {Object} parasiteID
+ * 
+ * @return Color
+ */
 TickStabilates.prototype.getColor = function(parasiteID) {
    var parasitesSize = window.d3.tickStabilatesObject.parasites.length;
    for(var x = 0; x < parasitesSize; x++) {
@@ -878,6 +1009,13 @@ TickStabilates.prototype.getColor = function(parasiteID) {
    }
 };
 
+/**
+ * This method returns the X coordinate corresponding to the specified date
+ * 
+ * @param {String} dateText
+ * 
+ * @return Integer
+ */
 TickStabilates.prototype.getXFromDate = function(dateText) {
    var date = new Date(dateText);
    var yearDiff = (window.d3.tickStabilatesObject.endDate.getYear() + 1) - window.d3.tickStabilatesObject.startDate.getYear();
@@ -893,6 +1031,13 @@ TickStabilates.prototype.getXFromDate = function(dateText) {
    return x;
 };
 
+/**
+ * This method returns the Y coordinate corresponding to a material
+ * 
+ * @param {Integer} materialID
+ * 
+ * @return Integer
+ */
 TickStabilates.prototype.getY = function(materialID) {
    var materialsSize = window.d3.tickStabilatesObject.frozenMaterial.length;
    var sectionHeight = window.d3.tickStabilatesObject.windowHeight / materialsSize;
@@ -911,6 +1056,15 @@ TickStabilates.prototype.getY = function(materialID) {
    }
 };
 
+/**
+ * This method returns the Y coordinate of a Y Axis separator
+ * 
+ * a Y Axis separator shows what area is represented by a frozen material
+ * 
+ * @param {Integer} materialID
+ * 
+ * @return Integer
+ */
 TickStabilates.prototype.getSeparatorY = function (materialID) {
    var materialsSize = window.d3.tickStabilatesObject.frozenMaterial.length;
    var sectionHeight = window.d3.tickStabilatesObject.windowHeight / materialsSize;
@@ -921,6 +1075,9 @@ TickStabilates.prototype.getSeparatorY = function (materialID) {
    }
 };
 
+/**
+ * This method switches the UI to either the dark or light theme depending on what the theme is currently
+ */
 TickStabilates.prototype.changeTheme = function () {
    if(window.d3.tickStabilatesObject.body.style("background-color")=="rgb(4, 17, 23)") {
       window.d3.tickStabilatesObject.body.transition()
